dd8610a00e080800a32290b60386cc51fff84706
Patrick K. Etienne <12497980+pketienne@users.noreply.github.com>
2015-12-27 21:11:31 -0600

    Rebuilt repository.

---
+++ roles/avalon/files/install_fedora.sh
@@ -0,0 +1,2 @@
+# call expect script for interactive section with fedora installer
+expect /opt/install/install_fedora.exp
---
+++ vaultify.py
@@ -0,0 +1,2 @@
+#! /bin/bash
+# write some script to keep from accidentally committing unencrypted sensitive files.
